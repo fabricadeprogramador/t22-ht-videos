560fcf54f50f1aaf674d96445ffc66425e64bc2a
camilarammos <camilarammos@gmail.com>
2019-03-02 08:27:30 -0400

    crie a barra de menu

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,88 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-layout
+    wrap
+    style="height: 200px;"
+  >
+    <v-container>
+      <v-layout justify-center>
+        <v-icon
+          
+          dark
+          @click.stop="drawer = !drawer"
+        
+        >
+          dehaze
+        </v-icon>
+      </v-layout>
+    </v-container>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="mini"
+      absolute
+      dark
+      temporary
+    >
+      <v-list class="pa-1">
+        <v-list-tile v-if="mini" @click.stop="mini = !mini">
+          <v-list-tile-action>
+            <v-icon>chevron_right</v-icon>
+          </v-list-tile-action>
+        </v-list-tile>
+
+        <v-list-tile avatar tag="div">
+          <v-list-tile-avatar>
+            <img src="https://randomuser.me/api/portraits/men/85.jpg">
+          </v-list-tile-avatar>
+
+          <v-list-tile-content>
+            <v-list-tile-title>John Leider</v-list-tile-title>
+          </v-list-tile-content>
+
+          <v-list-tile-action>
+            <v-btn icon @click.stop="mini = !mini">
+              <v-icon>chevron_left</v-icon>
+            </v-btn>
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list>
+
+      <v-list class="pt-0" dense>
+        <v-divider light></v-divider>
+
+        <v-list-tile
+          v-for="item in items"
+          :key="item.title"
+          
+        >
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </v-layout>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    data () {
+      return {
+        drawer: null,
+        items: [
+          { title: 'Home', icon: 'home' },
+          { title: 'Categorias', icon: 'style' },
+          { title: 'Vídeo', icon: 'movie_creation' }
+        ],
+        mini: false,
+        right: null
+      }
+    }
   }
-}
 </script>
 
 <style>
