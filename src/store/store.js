3019f1d656aa1b03ecb394caf247421617656fff
kennedylima <kennedyoliveiralima@gmail.com>
2019-05-25 08:30:29 -0400

    Adicionando o README.md como se faz para adicionar a origin do fabrica

--- src/store/store.js
+++ src/store/store.js
@@ -48,7 +48,7 @@ export default new Vuex.Store({
                 categoria.videos.forEach(video => {
                     acc.push(
                         video.titulo
-                    )    
+                    )
                 });
                
                 return acc;
