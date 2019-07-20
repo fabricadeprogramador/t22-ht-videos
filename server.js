5bfac51b6ff1fa054156b1c1153977756e412eee
strawkeiji <strawkeiji@gmail.com>
2019-07-20 11:54:41 -0400

    configurando ambiente de produção.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const endereco = '0.0.0.0';
 const porta = process.env.PORT || 5000;
 const server = express();
 
-server.use(serverStatic(path.join(__dirname, 'dist')));
+server.use(serveStatic(path.join(__dirname, 'dist')));
 server.listen(porta, endereco, () => {
     console.log("ht videos inicializada");
 })
