1a278c0d6174f6d8e06e8f72927811c41fbb5e4c
kennedylima <kennedyoliveiralima@gmail.com>
2019-07-20 11:09:02 -0400

    Configurando ambiente de producao

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const serveStatic = require('serve-static');
+const endereco = '0.0.0.0';
+const porta = process.env.PORT || 5000;
+const server = express();
+
+server.use(serveStatic(path.join(__dirname, 'dist')));
+
+server.listen(porta, endereco, () => {
+  console.log('HT Videos Inicializado');
+});
--- src/api.js
+++ src/api.js
@@ -1,4 +1,6 @@
-const url = 'http://localhost:3000/';
+const DEV = 'http://localhost:3000/';
+const PROD = 'https://ht-videos-api.herokuapp.com/';
+const url = process.env.NODE_ENV === 'production' ? PROD : DEV;
 export default { 
   url
 }
